1730c41be170548fa428ab80d129bfaff8b27446
Duran914 <jonathan.duran.100@my.csun.edu>
2018-01-08 23:36:42 -0800

    javascript code

--- app.js
+++ app.js
@@ -11,3 +11,26 @@ function dropdownClick() {
       x.className = 'navBar';
     }
 }
+
+// function for back to top button
+
+
+
+
+// function for navigation color change
+window.onscroll = function() {changeNavbarColor()};
+
+function changeNavbarColor() {
+  if (document.body.scrollTop > 16 ||
+  document.documentElement.scrollTop > 16){
+    document.getElementById('dropDownMenu').className =
+    "white-navBar", "white-navBar-li";
+    document.getElementById('lis').className =
+    "black-li";
+  }else {
+    document.getElementById('dropDownMenu').className =
+    "navBar";
+    document.getElementById('lis').className =
+    "white-li";
+  }
+}
