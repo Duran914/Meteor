fcec4282a23ca8eef42dde23ba02fa6a428cb095
Duran914 <jonathan.duran.100@my.csun.edu>
2018-01-10 14:57:11 -0800

    fixes

--- app.js
+++ app.js
@@ -18,19 +18,25 @@ function dropdownClick() {
 
 
 // function for navigation color change
-window.onscroll = function() {changeNavbarColor()};
-
-function changeNavbarColor() {
-  if (document.body.scrollTop > 16 ||
-  document.documentElement.scrollTop > 16){
-    document.getElementById('dropDownMenu').className =
-    "white-navBar", "white-navBar-li";
-    document.getElementById('lis').className =
-    "black-li";
-  }else {
-    document.getElementById('dropDownMenu').className =
-    "navBar";
-    document.getElementById('lis').className =
-    "white-li";
-  }
-}
+// window.onscroll = function() {changeNavbarColor()};
+//
+// function changeNavbarColor() {
+//
+//   if (document.body.scrollTop > 16 ||
+//   document.documentElement.scrollTop > 16){
+//     document.getElementById('dropDownMenu').className =
+//     "white-navBar";
+//   }else {
+//     document.getElementById('dropDownMenu').className =
+//     "navBar";
+//   }
+// }
+//
+// if (document.body.scrollTop > 16 ||
+// document.documentElement.scrollTop > 16){
+//   document.getElementById('navigationLinks').className =
+//   "white-navBar";
+// }else {
+//   document.getElementById('navigationLinks').className =
+//   "navBar";
+// }
