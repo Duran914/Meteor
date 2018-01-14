36676db757be14c244f94ce3a658f41e95155f53
Duran914 <jonathan.duran.100@my.csun.edu>
2018-01-14 01:43:39 -0800

    sticky navbar

--- app.js
+++ app.js
@@ -3,11 +3,56 @@
 //function for dropdown menu
 
 function dropdownClick() {
-    var x = document.getElementById('dropDownMenu');
+    let x = document.getElementById('dropDownMenu');
     if (x.className === 'navBar') {
         x.className += ' responsiveNavbar';
     }
     else {
       x.className = 'navBar';
     }
+}
+
+// scroll to nav links
+// function scrollToAbout() {
+//     let aboutSection = document.getElementById('servicesJs');
+//     aboutSection.scrollIntoView();
+//     prevent.default();
+// }
+
+function scrollToSection() {
+  // nav ids
+  let home = document.getElementById('homeLink');
+  let about = document.getElementById("aboutLink");
+  let offers = document.getElementById('offersLink');
+  let portfolio = document.getElementById('portfolioLink');
+  let blog = document.getElementById('blogLink');
+  let contactUs = document.getElementById('contactLink');
+
+  //nav sections
+  let showcaseSection = document.getElementById('showcaseJs');
+  let aboutSection = document.getElementById('servicesJs');
+  let offersSection = document.getElementById('offersJs');
+  let portfolioSection = document.getElementById('portfolioJs');
+  let blogSection = document.getElementById('blogJs');
+  let contactUsSection = document.getElementById("contact-usJs");
+
+  if (about) {
+     aboutSection.scrollIntoView();
+  }
+  else if (home) {
+    showcaseSection.scrollIntoView();
+  }
+  else if (offers) {
+    offersSection.scrollIntoView();
+  }
+  else if (portfolio) {
+    portfolioSection.scrollIntoView();
+  }
+  else if (blog) {
+    blogSection.scrollIntoView();
+  }
+  else if (contactUs) {
+    contactUsSection.scrollIntoView();
+  }
+
 }
